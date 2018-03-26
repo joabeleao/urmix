de314add3578ec80f5f5e2809b0bf633ccfb29fc
Joabe Leão <31323960+joabeleao@users.noreply.github.com>
2018-03-26 12:24:23 -0300

    Update haste.sh

--- haste.sh
+++ haste.sh
@@ -7,13 +7,17 @@ TIMESTYLE="$(date +'%Y-%m-%d %H:%M:%S')"
 
 while true; do
 
+  # Get available memory with free in standart kilobytes
   MEMAV=$(free | awk 'FNR == 2 {print $7}' | grep -E [[:digit:]])
-  
-  if [[ ${MEMAV} -lt 400000 ]]; then
+  # Convert output to megabytes
+  MEMAVMB=$(( ${MEMAV} / 1024 ))
 
+  if [[ ${MEMAV} -lt 250000 ]]; then
+
+    # For future, add check if chrome exists, after, add check to close another mem suckers
     killall chrome
-    echo "${TIMESTYLE} : There is no free memory available for continue to working."
-    echo "${TIMESTYLE} : The available amount of memory is ${MEMAV}"
+    echo "${TIMESTYLE} : There is no free memory available for continue to working." >> ${LOG} 2>&1
+    echo "${TIMESTYLE} : The available amount of memory is ${MEMAVMB}MB" >> ${LOG} 2>&1
     echo "${TIMESTYLE} : The chrome process will now be closed" >> ${LOG} 2>&1
 
     # To free pagecache:
